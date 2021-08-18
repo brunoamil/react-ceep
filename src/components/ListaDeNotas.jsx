26b70f0a9d375f3aa6c0ef8ff222989a369be451
bruno-amil@hotmail.com <bruno-amil@hotmail.com>
2021-08-18 10:42:13 -0300

    teestando o token

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -17,7 +17,7 @@ export default class ListaDeNotas extends Component {
             <header>
               <h3>Título</h3>
             </header>
-            <p>Escreva sua nota</p>
+            <p>Escreva sua notsa</p>
           </section>
         </li>
         <li>
